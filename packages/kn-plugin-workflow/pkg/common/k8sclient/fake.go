3d9c1053906d2a6cb4f400d0e4d1dc99c397bd06
Dmitrii Tikhomirov <chani.liet@gmail.com>
2024-08-30 01:47:20 -0700

    it must be possible to apple yaml with multiple declarations

--- packages/kn-plugin-workflow/pkg/common/k8sclient/fake.go
+++ packages/kn-plugin-workflow/pkg/common/k8sclient/fake.go
@@ -1,108 +1,40 @@
 package k8sclient
 
 import (
-	"context"
 	"fmt"
 	"github.com/spf13/afero"
-	clientsetfake "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset/fake"
-	"k8s.io/apimachinery/pkg/api/meta"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/yaml"
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/dynamic/fake"
-	"k8s.io/client-go/kubernetes/scheme"
+	"strings"
 )
 
 type Fake struct {
 	FS afero.Fs
 }
 
-func (m Fake) GetNamespace() (string, error) {
-	return "default", nil
-}
-
-func (m Fake) CheckContext() (string, error) {
-	return "default", nil
-}
-
-func (m Fake) ExecuteApply(path, namespace string) error {
-	data, err := afero.ReadFile(m.FS, path)
-	if err != nil {
-		return fmt.Errorf("❌ ERROR: Failed to read YAML file: %w", err)
-	}
-
-	obj, _, err := scheme.Codecs.UniversalDeserializer().Decode(data, nil, nil)
-	if err != nil {
-		return fmt.Errorf("❌ ERROR: Failed to decode YAML: %w", err)
-	}
-
-	unstructuredObj, err := runtime.DefaultUnstructuredConverter.ToUnstructured(obj)
-	if err != nil {
-		return fmt.Errorf("❌ ERROR: Failed to decode YAML: %w", err)
-	}
-
-	unstructuredMap := &unstructured.Unstructured{Object: unstructuredObj}
-	gvk := unstructuredMap.GroupVersionKind()
-	gvr, _ := meta.UnsafeGuessKindToResource(gvk)
-
-	fakeDynamic, err := getFakeDynamicClient()
-	if err != nil {
-		return fmt.Errorf("❌ ERROR: Failed to create dynamic fake Kubernetes client: %v", err)
-	}
-
-	_, err = fakeDynamic.Resource(gvr).Namespace(namespace).Create(context.Background(), unstructuredMap, metav1.CreateOptions{})
-	if err != nil {
-		return fmt.Errorf("❌ ERROR: Failed to create resource: %w", err)
-	}
-	return nil
-}
-
-func (m Fake) ExecuteDelete(path, namespace string) error {
-	data, err := afero.ReadFile(m.FS, path)
-	if err != nil {
-		return fmt.Errorf("❌ ERROR: Failed to read YAML file: %w", err)
-	}
-
-	obj, _, err := scheme.Codecs.UniversalDeserializer().Decode(data, nil, nil)
-	if err != nil {
-		return fmt.Errorf("❌ ERROR: Failed to decode YAML: %w", err)
-	}
-
-	unstructuredObj, err := runtime.DefaultUnstructuredConverter.ToUnstructured(obj)
-	if err != nil {
-		return fmt.Errorf("❌ ERROR: Failed to decode YAML: %w", err)
-	}
-
-	unstructuredMap := &unstructured.Unstructured{Object: unstructuredObj}
-	gvk := unstructuredMap.GroupVersionKind()
-	gvr, _ := meta.UnsafeGuessKindToResource(gvk)
-
-	fakeDynamic, err := getFakeDynamicClient()
-	if err != nil {
-		return fmt.Errorf("❌ ERROR: Failed to create dynamic fake Kubernetes client: %v", err)
-	}
-
-	deletePolicy := metav1.DeletePropagationForeground
-
-	err = fakeDynamic.Resource(gvr).Namespace(namespace).Delete(context.Background(), unstructuredMap.GetName(), metav1.DeleteOptions{
-		PropagationPolicy: &deletePolicy,
-	})
-
-	return nil
-}
-
-func (m Fake) CheckCrdExists(crd string) (bool, error) {
-	fakeClientSet := clientsetfake.NewSimpleClientset()
-	_, err := fakeClientSet.ApiextensionsV1().CustomResourceDefinitions().Get(context.Background(), crd, metav1.GetOptions{})
-	if err != nil {
-		return false, err
-	}
-	return true, nil
-}
-
-func getFakeDynamicClient() (dynamic.Interface, error) {
+func (m Fake) FakeDynamicClient() (dynamic.Interface, error) {
 	scheme := runtime.NewScheme()
 	fakeDynamicClient := fake.NewSimpleDynamicClient(scheme)
 	return fakeDynamicClient, nil
+}
+
+func (m Fake) FakeParseYamlFile(path string) ([]unstructured.Unstructured, error) {
+	data, err := afero.ReadFile(m.FS, path)
+	if err != nil {
+		return nil, fmt.Errorf("❌ ERROR: Failed to read YAML file: %w", err)
+	}
+	decoder := yaml.NewYAMLOrJSONDecoder(strings.NewReader(string(data)), 4096)
+	result := []unstructured.Unstructured{}
+	for {
+		rawObj := &unstructured.Unstructured{}
+		err := decoder.Decode(rawObj)
+		if err != nil {
+			break
+		}
+		result = append(result, *rawObj)
+	}
+	return result, nil
 }
